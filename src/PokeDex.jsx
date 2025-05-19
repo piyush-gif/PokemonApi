1283a157d3775322c67b4be87b7b3480da1ed1e9
piyush <piyush.khadka2233@gmail.com>
2025-05-19 22:55:05 +0100

    added delete functionality to pokedex

--- src/PokeDex.jsx
+++ src/PokeDex.jsx
@@ -10,11 +10,12 @@ const PokeDex = () => {
     .then((res) => {
       if(!res) {
         throw new Error('Failed to fetch the data');
+        
       }
       return res.json();
     })
     .then((data) => {
-      setCard(data);
+      setCard(data.pokemons || data);
       setLoading(false);
     })
     .catch((err) =>{
@@ -24,15 +25,32 @@ const PokeDex = () => {
   },[])
    if (loading) return <div>Loading Pokémon...</div>;
   if (error) return <div>Error: {error}</div>;
+
+  const handleDelete= (id) => {
+    fetch(`http://localhost:8000/pokemons/${id}`,{
+      method: 'DELETE'
+    })
+    .then((res) =>{
+      if(!res.ok){
+        throw new Error ('Delete Failed');
+        
+      }setCard(cards.filter(card => card.id !== id));
+    }) 
+    .catch(err => {
+      console.error('Delete error:', err);
+      setError('Failed to delete Pokémon');
+    });
+  }
   return ( 
     <div className="pokedex-container">
-      {cards.map((card , id) => (
-        <div className="pokemon-card" key={id}>
+      {cards.map((card) => (
+        <div className="pokemon-card" key={card.id || card.name}>
           <h3>{card.name}</h3>
           <p>Height: {card.height}</p> 
           <p>Weight: {card.weight}</p> 
           <p>Base XP: {card.base_experience}</p>
           <p>Type: <span className={`type-${card.type}`}>{card.type}</span></p>
+          <button onClick={() => handleDelete(card.id)}>delete</button>
         </div>
       ))}
     </div>
