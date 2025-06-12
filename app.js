d8ffe74d98423adacef95d4561b584d246041220
piyush <piyush.khadka2233@gmail.com>
2025-06-12 22:06:20 +0100

    initial server running

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-import express from 'express';
+import express, { application } from 'express';
 
 
 const app = express();
-
-
 
 app.get('/about-us', (req, res) => {
   res.send('<p>hello it works</p>');
 })
 
 
-app.listen(9000);
+app.get('/home')
+
+app.listen(5000);
