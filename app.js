1ec8036ed972b7c871db7602b74f979d8a13ab0b
piyush <piyush.khadka2233@gmail.com>
2025-06-13 23:52:58 +0100

    mongodb URI

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ import express, { application } from 'express';
 
 const app = express();
 
+//connect to mongodb
+
+const dbURI = 'mongodb+srv://piyush:test1234@cluster0.w93cmfv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+app.get('/', (req, res) => {
+  res.send('')
+})
 app.get('/about-us', (req, res) => {
   res.send('<p>hello it works</p>');
 })
