51d637e9e4914fa6977be00f4653be96c2eaf5ad
piyush <piyush.khadka2233@gmail.com>
2025-11-18 13:37:56 +0000

    client and server

---
+++ client/src/PokeDex.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+
+const PokeDex = () => {
+  return (
+    <div>
+      <div>
+        <p>hello this is the PokeDex</p>
+      </div>
+    </div>
+  )
+};
+
+export default PokeDex;
--- src/PokeDex.jsx
+++
@@ -1,7 +0,0 @@
-import { useEffect, useState } from 'react';
-
-const PokeDex = () => {
-  
-};
-
-export default PokeDex;
