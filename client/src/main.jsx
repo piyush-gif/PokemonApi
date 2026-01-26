6ab5b7aa5c9ca57204ab90f839c1c98b4cf1595b
piyush <piyush.khadka2233@gmail.com>
2026-01-26 19:42:46 +0000

    full clear

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '../style/navBar.css'
-import '../style/homePage.css'
-import '../style/pokeDex.css'
-import '../style/index.css'
-import '../style/pokemoncard.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-        <App />
+    <App />
   </StrictMode>,
-)
+);
